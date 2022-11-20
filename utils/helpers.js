25b7a115c85fdde0fa1d26c21af79ca496947e79
Todor Imreorov <blurymind@gmail.com>
2022-11-20 17:39:02 +0000

    add initial kaboom load

--- utils/helpers.js
+++ utils/helpers.js
@@ -308,3 +308,92 @@ export const initPwaLogic = () => {
         console.log('PWA was installed');
     });
 }
+
+/////// KABOOM JS
+
+// Todo export feature
+export default function genGame(code) {
+    return `
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>kaboom</title>
+	<meta charset="utf-8">
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+		}
+		html,
+		body {
+			width: 100%;
+			height: 100%;
+			overflow: hidden;
+		}
+	</style>
+</head>
+
+<body>
+	<script src="/lib/dev/kaboom.js"></script>
+	<script>
+${code}
+	</script>
+</body>
+
+</html>
+	`;
+}
+
+const generateResources = (resources = []) => {
+    // console.log(resources);
+    const resBoilerplate = resources
+        .map((resource, index) => {
+            if (!resource.file) return '';
+            const resourceName = resource.name || index;
+            if (resource.file.startsWith('data:image/') || resource.file.endsWith('.png'))
+                return `loadSprite("${resourceName}", "${resource.file}");`;
+            if (resource.file.startsWith('data:audio/') || resource.file.endsWith('.wav') )
+                return `loadSound("${resourceName}", "${resource.file}");`;
+            return '';
+        })
+        .join('\n');
+    // console.log('CODE:', resBoilerplate);
+    return resBoilerplate;
+};
+export const genPreview = (code, resources = []) => {
+    return `
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>kaboom</title>
+	<meta charset="utf-8">
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+		}
+		html,
+		body {
+			width: 100%;
+			height: 100%;
+			overflow: hidden;
+		}
+	</style>
+</head>
+
+<body>
+	<script src="kaboom/kaboom.js"></script>
+	<script>
+    ${code}
+    ${generateResources(resources)}
+	</script>
+	<canvas id="kaboomCanvas"></canvas>
+</body>
+
+</html>
+	`;
+};
